21ff6302671d7847d5001b5ecdb065874d82ba2e
Leg3ndary <ben.zhou.2006@gmail.com>
2024-12-03 13:21:23 -0500

    Finish day3

--- 2024/day3.cpp
+++ 2024/day3.cpp
@@ -6,17 +6,65 @@ void setup() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    freopen("tests/dayx.txt", "r", stdin);
+    freopen("tests/day3.txt", "r", stdin);
     cin.clear();
     fseek(stdin, 0, SEEK_SET);
 }
 
 void problem1() {
+    string input;
+    string temp;
+    while (getline(cin, temp)) {
+        input += temp;
+    }
+    regex pattern(R"(mul\((\d{1,3}),(\d{1,3})\))");
+    smatch match;
+    long long sum = 0;
 
+    auto it = input.cbegin();
+    while (regex_search(it, input.cend(), match, pattern)) {
+        int x = stoi(match[1].str());
+        int y = stoi(match[2].str());
+        sum += x * y;
+        it = match.suffix().first;
+    }
+
+    cout << "Sum of all results: " << sum << "\n";
 }
 
 void problem2() {
+    string input;
+    string temp;
 
+    while (getline(cin, temp)) {
+        input += temp;
+    }
+
+    regex combinedPattern(R"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))");
+    smatch match;
+    long long sum = 0;
+    bool enabled = true;
+
+    auto it = input.cbegin();
+    while (regex_search(it, input.cend(), match, combinedPattern)) {
+        string matched = match.str();
+
+        if (matched.find("mul") == 0) {
+            if (enabled) {
+                int x = stoi(match[1].str());
+                int y = stoi(match[2].str());
+                sum += x * y;
+            }
+        } else if (matched == "do()") {
+            enabled = true;
+        } else if (matched == "don't()") {
+            enabled = false;
+        }
+
+        it = match.suffix().first;
+    }
+
+    cout << "Sum of all results: " << sum << "\n";
 }
 
 int main() {
