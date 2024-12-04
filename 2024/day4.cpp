500b8e8c14d30e917563f93f61ae80172fb938ac
Leg3ndary <ben.zhou.2006@gmail.com>
2024-12-03 20:32:41 -0500

    Create template files for the next problem

---
+++ 2024/day4.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void setup() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    freopen("tests/day4.txt", "r", stdin);
+    cin.clear();
+    fseek(stdin, 0, SEEK_SET);
+}
+
+void problem1() {
+
+}
+
+void problem2() {
+
+}
+
+int main() {
+    setup();
+    problem1();
+    setup();
+    problem2();
+    return 0;
+}
